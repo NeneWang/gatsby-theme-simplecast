4d8cc36530432116f236e500090167d140dca0ea
NelsonWang <wangnelson2@gmail.com>
2022-04-29 18:27:38 -0400

    artists

--- demo/src/pages/artists.js
+++ demo/src/pages/artists.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Img from "gatsby-image"
 
 export default function NotFound() {
-  return (
-    <div>
-      <h1>Artists Page</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Artists Page</h1>
+            <img src="https://www.mcmireport.com/wp-content/uploads/2021/03/MCMI-PEN-SWORD-RECORDS.jpg" alt="" />
+            <Img src="../../static/img/2022-04-29-18-24-16.png" alt="" />
+            <li>Keith WildChild Middleton</li>
+            <li>LR Blitzkrieg</li>
+            <li>GMS	GMS</li>
+            <li>PH	PH</li>
+            <li>CHORDZ CORDERO in STUDIO	CHORDZ CORDERO</li>
+            <li>Artists	Juggernaut War Party</li>
+            <li>HYDRA	HYDRA</li>
+            <li>plague	ThePLAGUE (-aka- ‘Army of the 12Monkeyz’)</li>
+            <li>MarXman	The MarXman</li>
+            <li>MrMecc	MrMecc</li>
+        </div>
+    )
 }
---
+++ demo/src/pages/podcasts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Podcasts</h1>
+      
+      <iframe width="1038" height="593" src="https://www.youtube.com/embed/V1wO_eggtpw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+    </div>
+  )
+}
